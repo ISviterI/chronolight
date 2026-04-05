d0e2c1397777353f2d40edd06301c0af228de7ca
ISviterI <clashtop.company@gmail.com>
2026-04-05 18:57:35 +0300

    HOTFIX

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chronolight",
-    version="1.1.1",
+    version="1.1.2",
     author="Sviter",
     description="A simple library for easily working with time",
     url="https://github.com/ISviterI/chronolight",
