bdc574fc9a1e7d878487a301d018c4b4c244103d
ISviterI <clashtop.company@gmail.com>
2026-04-07 20:04:48 +0300

    just small qol

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chronolight",
-    version="1.4.3",
+    version="1.4.4",
     author="Sviter",
     description="A simple library for easily working with time",
     url="https://github.com/ISviterI/chronolight",
@@ -21,12 +21,5 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
-    entry_points={
-        #"console_scripts": [
-        #    "chronolight=chronolight.chronolight_cli:app",
-        #    "cl=chronolight.chronolight_cli:app",
-        #]
-    },
     keywords="chronolight,chrono,timelines,delay,time,timeline,after,after_delay,afterdelay,light,chronos,simple,easy,easytoworkwith,easy_to_work_with,very_simple,cool,peak,bruh,lol,chains,chain,class,working_with_time",
-    install_requires=['typer','plyer',"tqdm"]
 )
